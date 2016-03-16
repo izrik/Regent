8e468917456f8d195bc2c90c92b9656199ac820b
izrik <izrik@izrik.com>
2016-03-16 11:14:41 -0500

    Initial project structure.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Regent")]
+[assembly: AssemblyDescription("A Common Language Runtime written in C#")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("izrik")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1.*")]
+
+
---
+++ Regent.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Regent
+{
+    public class Regent
+    {
+    }
+}
+
